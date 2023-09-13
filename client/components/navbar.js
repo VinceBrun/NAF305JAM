a946c16f3113ee546b2684849869117f7ef9b743
vincebrun <ambavincent1203@gmail.com>
2023-09-13 13:38:11 +0100

    commit

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,24 +1,20 @@
 import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
-import Image from "next/image"
+import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
 import Register from "./register";
-import Home from "../pages/index.js"
-
 import { ConnectWallet } from "@thirdweb-dev/react";
-
+import { useContract, useContractRead } from "@thirdweb-dev/react"; // Import useContract and useContractRead
 
 const Navbar = () => {
-  const navigation = [
-    "Home",
-    "Blog",
-    <Register />
-  ];
+  const navigation = ["Home", "Blog", <Register />];
+  const { contract } = useContract("0xA8D27E5a646e20365B8F15033220f861d5DAEAD4");
+  const { data, isLoading } = useContractRead(contract, "admin", []); // Use the useContractRead hook to read the "admin" function
 
   return (
     <div className="w-full">
       <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
-        {/* Logo  */}
+        {/* Logo */}
         <Disclosure>
           {({ open }) => (
             <>
@@ -40,11 +36,13 @@ const Navbar = () => {
 
                 <Disclosure.Button
                   aria-label="Toggle Menu"
-                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700">
+                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
+                >
                   <svg
                     className="w-6 h-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24">
+                    viewBox="0 0 24 24"
+                  >
                     {open && (
                       <path
                         fillRule="evenodd"
@@ -62,21 +60,22 @@ const Navbar = () => {
                 </Disclosure.Button>
 
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
-                  
-                    <ConnectWallet />
-                  
+                  <ConnectWallet />
                 </Disclosure.Panel>
               </div>
             </>
           )}
         </Disclosure>
 
-        {/* menu  */}
+        {/* menu */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
-                <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                <Link
+                  href="/"
+                  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
+                >
                   {menu}
                 </Link>
               </li>
@@ -86,11 +85,16 @@ const Navbar = () => {
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
           <ConnectWallet />
-            <ThemeChanger />
+          <ThemeChanger />
         </div>
       </nav>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : data === "admin" ? (
+        <div>Welcome to the Admin Dashboard!</div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default Navbar;
